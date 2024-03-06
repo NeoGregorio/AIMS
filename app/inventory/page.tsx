4a6e97e22eb69d09c957d53d249196cd653300ab
inunice <ammanguan@up.edu.ph>
2024-03-06 14:28:32 +0800

    feat: implement add item pop up

--- app/inventory/page.tsx
+++ app/inventory/page.tsx
@@ -1,6 +1,7 @@
 import { createClient } from "@/utils/supabase/client";
 import NavBar from "@/components/NavBar";
 import SearchBar from "@/components/SearchBar";
+import { AddItem } from "@/components/AddItem";
 
 export default function Main({
   searchParams,
@@ -62,21 +63,7 @@ export default function Main({
             <span>Category</span>
           </div>
         </div>
-        <button className="text-sm flex flex-row gap-2 items-center ease-in-out duration-200 shadow-lg leading-4 text-center py-4 px-6 flex rounded-full no-underline font-semibold bg-btn-background text-white hover:bg-btn-background-hover transition-colors duration-200">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            className="w-4 h-4"
-          >
-            <path
-              fillRule="evenodd"
-              d="M12 3.75a.75.75 0 0 1 .75.75v6.75h6.75a.75.75 0 0 1 0 1.5h-6.75v6.75a.75.75 0 0 1-1.5 0v-6.75H4.5a.75.75 0 0 1 0-1.5h6.75V4.5a.75.75 0 0 1 .75-.75Z"
-              clipRule="evenodd"
-            />
-          </svg>
-          <span>Add new item</span>
-        </button>
+        <AddItem />
       </div>
     </div>
   );
---
+++ components/AddItem.tsx
@@ -0,0 +1,66 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+
+export function AddItem() {
+  return (
+    <Dialog>
+      <DialogTrigger className="text-sm flex flex-row gap-2 items-center ease-in-out duration-200 shadow-lg leading-4 text-center py-4 px-6 flex rounded-full no-underline font-semibold bg-btn-background text-white hover:bg-btn-background-hover transition-colors duration-200">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          className="w-4 h-4"
+        >
+          <path
+            fillRule="evenodd"
+            d="M12 3.75a.75.75 0 0 1 .75.75v6.75h6.75a.75.75 0 0 1 0 1.5h-6.75v6.75a.75.75 0 0 1-1.5 0v-6.75H4.5a.75.75 0 0 1 0-1.5h6.75V4.5a.75.75 0 0 1 .75-.75Z"
+            clipRule="evenodd"
+          />
+        </svg>
+        <span>Add new item</span>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Add new item</DialogTitle>
+          <DialogDescription>
+            Add item details here; click save when you're done!
+          </DialogDescription>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="name" className="text-right">
+              Item Name
+            </Label>
+            <Input id="name" value="" className="col-span-3" />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="price" className="text-right">
+              Category
+            </Label>
+            <Input id="category" value="" className="col-span-3" />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="price" className="text-right">
+              Price
+            </Label>
+            <Input id="price" value="" className="col-span-3" />
+          </div>
+        </div>
+        <DialogFooter>
+          <Button type="submit">Save changes</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
